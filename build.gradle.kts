7c4548eabbb975115ff5160f3c9a456ce495ae2f
statking <60002940+kimwoonggon@users.noreply.github.com>
2026-04-06 11:30:08 +0900

    ktor + netty basic

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,4 +24,10 @@ tasks.test {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.7.3")
+
+    // Ktor Server
+    implementation("io.ktor:ktor-server-core-jvm:2.3.12")
+    implementation("io.ktor:ktor-server-netty-jvm:2.3.12")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:2.3.12")
+    implementation("io.ktor:ktor-serialization-gson-jvm:2.3.12")
 }
